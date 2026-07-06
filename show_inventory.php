c0fc8c1f8390c2517168ac11ff067052d50a3309
Greg H <grarha@gmail.com>
2026-07-05 23:17:44 -0700

    Boilerplate HTML & PHP, smoke tested

---
+++ show_inventory.php
@@ -0,0 +1,12 @@
+<!--CS 85 Module 4, Assignment 4B by Gregory Hagen 7/5/26-->
+<!DOCTYPE HTML>
+<html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Personal Inventory</title>
+    </head>
+<body>
+    <?php
+    ?>
+</body>
+</html>
